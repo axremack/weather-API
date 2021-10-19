c02cb2dcc2170881e2585bbada1ec4abdbe80aea
uke <ax.remack@gmail.com>
2021-10-19 19:35:13 +0200

    :construction: Adding constructor and getters/setters for city weather

--- src/city_weather/CityWeather.java
+++ src/city_weather/CityWeather.java
@@ -1,20 +1,74 @@
 package city_weather;
 
 public class CityWeather {
+    // Paramètres obligatoires
+    private GeneralWeather general_weather;
     private Temperature temp;
     private Wind wind;
-
-    // General infos
-    private String weather_type;
-    private String weather_description;
-
-    private String station;
-
+    private Rain rain;
+    private Snow snow;
     private double cloudiness;
 
-    private double snow_1h;
-    private double snow_3h;
+    // Constructeur
+    public CityWeather(GeneralWeather general_weather, Temperature temp, Wind wind, Rain rain, Snow snow, double cloudiness) {
+        this.general_weather = general_weather;
+        this.temp = temp;
+        this.wind = wind;
+        this.rain = rain;
+        this.snow = snow;
+        this.cloudiness = cloudiness;
+    }
 
-    // Constructeurs
+    // Getters et setters
+    public GeneralWeather getGeneral_weather() {
+        return general_weather;
+    }
+
+    public void setGeneral_weather(GeneralWeather general_weather) {
+        this.general_weather = general_weather;
+    }
+
+    public Temperature getTemp() {
+        return temp;
+    }
+
+    public void setTemp(Temperature temp) {
+        this.temp = temp;
+    }
+
+    public Wind getWind() {
+        return wind;
+    }
+
+    public void setWind(Wind wind) {
+        this.wind = wind;
+    }
+
+    public Rain getRain() {
+        return rain;
+    }
+
+    public void setRain(Rain rain) {
+        this.rain = rain;
+    }
+
+    public Snow getSnow() {
+        return snow;
+    }
+
+    public void setSnow(Snow snow) {
+        this.snow = snow;
+    }
+
+    public double getCloudiness() {
+        return cloudiness;
+    }
+
+    public void setCloudiness(double cloudiness) {
+        this.cloudiness = cloudiness;
+    }
+
+
+
 
 }
