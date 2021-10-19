d7ef2a2eb3aa44ab1beeae9874633e9bb3debf26
uke <ax.remack@gmail.com>
2021-10-19 19:22:15 +0200

    :sparkles: Adding class for rain informations

---
+++ src/city_weather/Rain.java
@@ -0,0 +1,31 @@
+package city_weather;
+
+public class Rain {
+    // Paramètres obligatoires
+    private double volume_1h;
+    private double volume_3h;
+
+    // Constructeur
+    public Rain(double volume_1h, double volume_3h) {
+        this.volume_1h = volume_1h;
+        this.volume_3h = volume_3h;
+    }
+
+    // Getters et setters
+    public double getVolume_1h() {
+        return volume_1h;
+    }
+
+    public void setVolume_1h(double volume_1h) {
+        this.volume_1h = volume_1h;
+    }
+
+    public double getVolume_3h() {
+        return volume_3h;
+    }
+
+    public void setVolume_3h(double volume_3h) {
+        this.volume_3h = volume_3h;
+    }
+
+}
