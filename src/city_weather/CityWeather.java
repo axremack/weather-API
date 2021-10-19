e63e82129c5c4b5a19f84e272815b4c708ee8b8a
uke <ax.remack@gmail.com>
2021-10-19 13:54:47 +0200

    :construction: Adding class for a city weather

---
+++ src/city_weather/CityWeather.java
@@ -0,0 +1,20 @@
+package city_weather;
+
+public class CityWeather {
+    private Temperature temp;
+    private Wind wind;
+
+    // General infos
+    private String weather_type;
+    private String weather_description;
+
+    private String station;
+
+    private double cloudiness;
+
+    private double snow_1h;
+    private double snow_3h;
+
+    // Constructeurs
+
+}
